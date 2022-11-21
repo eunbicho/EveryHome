ce3638272d0c1641612dadc01452317481ae11e1
choeunbi <silverain_9@naver.com>
2022-11-21 17:47:09 +0900

    [FEAT]: 게시판 라우터 내용 추가, 게시판 목록 검색기능 추가

--- src/views/BoardView.vue
+++ src/views/BoardView.vue
@@ -1,16 +1,14 @@
 <template>
   <div>
     <board-banner-comp></board-banner-comp>
-    <prop-search-comp></prop-search-comp>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import PropSearchComp from "@/components/Board/PropSearchComp.vue";
 import BoardBannerComp from "@/components/Board/BoardBannerComp.vue";
 export default {
   components: {
-    PropSearchComp,
     BoardBannerComp,
   },
 };
