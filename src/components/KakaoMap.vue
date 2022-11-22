7b91941792c77b705caee29c0dec2e5f98b0b25c
이재원 <ljaewon97@gmail.com>
2022-11-22 16:43:25 +0900

    [ADD] kakaomap
    20221122 1643

--- src/components/KakaoMap.vue
+++ src/components/KakaoMap.vue
@@ -50,7 +50,8 @@ export default {
           this.geocoder.addressSearch(road, function (result, status) {
             // 정상적으로 검색이 완료됐으면
             if (status === kakao.maps.services.Status.OK) {
-              let temp = `<div>${housedeal.buildingName}</div>`;
+              let temp = `<div style='position: absolute; left: 0px; top: 0px;'><div style='width:140px;padding:1px;text-align:center; font-weight: bold;'>${housedeal.buildingName}</div></div>`;
+              //let temp = `<div style="border-radius: 20px;">${housedeal.buildingName}</div>`;
               // positions.push({
               //   content: temp,
               //   latlng: new kakao.maps.LatLng(result[0].y, result[0].x),
@@ -116,7 +117,6 @@ export default {
         // 마커에 표시할 인포윈도우를 생성합니다
         var infowindow = new kakao.maps.InfoWindow({
           content: contents[i], // 인포윈도우에 표시할 내용
-          removable: true,
         });
 
         // 마커에 mouseover 이벤트와 mouseout 이벤트를 등록합니다
@@ -154,26 +154,6 @@ export default {
           infowindow.close();
         };
       }
-    },
-    displayInfoWindow() {
-      if (this.infowindow && this.infowindow.getMap()) {
-        //이미 생성한 인포윈도우가 있기 때문에 지도 중심좌표를 인포윈도우 좌표로 이동시킨다.
-        this.map.setCenter(this.infowindow.getPosition());
-        return;
-      }
-
-      var iwContent = '<div style="padding:5px;">Hello World!</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-        iwPosition = new kakao.maps.LatLng(33.450701, 126.570667), //인포윈도우 표시 위치입니다
-        iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
-
-      this.infowindow = new kakao.maps.InfoWindow({
-        map: this.map, // 인포윈도우가 표시될 지도
-        position: iwPosition,
-        content: iwContent,
-        removable: iwRemoveable,
-      });
-
-      this.map.setCenter(iwPosition);
     },
   },
 };
